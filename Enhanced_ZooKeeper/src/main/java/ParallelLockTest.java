475a8ae3c4914f5d149ddc4427f4b64851d715c9
Jayfunf <simh3077@gmail.com>
2025-06-03 04:26:22 +0900

    Update: 강제로 Watch 누락이 발생하는 Zookeeper 코드 추가

--- Enhanced_ZooKeeper/src/main/java/ParallelLockTest.java
+++ Enhanced_ZooKeeper/src/main/java/ParallelLockTest.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -13,11 +14,7 @@ import org.apache.zookeeper.ZooKeeper;
 
 public class ParallelLockTest {
 	private static final String LOCK_PATH = "/my-lock";
-	private static final int CLIENT_COUNT = 150;
-	private static final List<Long> elapsedTimes = Collections.synchronizedList(new ArrayList<>());
-	private static final List<String> acquiredNodeNames = Collections.synchronizedList(new ArrayList<>());
-	private static final AtomicInteger successCount = new AtomicInteger(0); // 락 획득 성공 수
-	private static final List<Integer> retryCounts = Collections.synchronizedList(new ArrayList<>()); // 각 클라이언트의 재시도 횟수 기록
+	private static final int CLIENT_COUNT = 2;
 
 	public static void main(String[] args) throws Exception {
 		TestingServer server = new TestingServer();
@@ -35,28 +32,22 @@ public class ParallelLockTest {
 			final int clientId = i;
 			executor.submit(() -> {
 				try {
-					ZooKeeper zk = new ZooKeeper(server.getConnectString(), 3000, null);
-					BasicZooKeeperLock lock = new BasicZooKeeperLock(zk, LOCK_PATH); // 개선 전 사용 시
-					// ImprovedZooKeeperLock lock = new ImprovedZooKeeperLock(zk, LOCK_PATH); // 개선 후 사용 시
+					ZooKeeper zk = new ZooKeeper(server.getConnectString(), 2000, null);
 
-					long startTime = System.nanoTime();
-					String myNode = lock.lock();
-					long elapsedTime = System.nanoTime() - startTime;
+					// BasicZooKeeperLock lock = new BasicZooKeeperLock(zk, LOCK_PATH); // 개선 전 사용 시
+					// ImprovedZooKeeperLock lock = new ImprovedZooKeeperLock(zk, LOCK_PATH); // 개선 후 사용 시
+					String role = (clientId == 0) ? "C1" : "C2";
+					SimpleZookeeperLock lock = new SimpleZookeeperLock(zk, LOCK_PATH, role); // Watch 누락 테스트
+
+					String myNode = lock.lock(); // SimpleLock 코드
 
 					if (myNode != null) {
-						String nodeName = myNode.substring(myNode.lastIndexOf("/") + 1);
-
-						elapsedTimes.add(elapsedTime);
-						acquiredNodeNames.add(nodeName);
-						retryCounts.add(lock.getRetryCount()); // 재시도 횟수 기록
-						successCount.incrementAndGet();
-						System.out.println("[Client " + clientId + "] 락 획득: " + nodeName +
-							" (대기 시간 ns): " + elapsedTime +
-							" (재시도 횟수): " + lock.getRetryCount());
+						System.out.println("LockTest - [" + role + "] 락 획득 성공: " + myNode);
+						lock.unlock();
+					} else {
+						System.out.println("LockTest - [" + role + "] 락 획득 실패 (watch 등록 후 대기 없음)");
 					}
 
-					Thread.sleep(500); // 임계구역 점유
-					lock.unlock();
 					zk.close();
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -70,23 +61,23 @@ public class ParallelLockTest {
 		executor.shutdown();
 		server.close();
 
-		System.out.println("모든 클라이언트 종료");
+		// 로그
+		// long total = elapsedTimes.stream().mapToLong(Long::longValue).sum();
+		// long max = elapsedTimes.stream().mapToLong(Long::longValue).max().orElse(0);
+		// long min = elapsedTimes.stream().mapToLong(Long::longValue).min().orElse(0);
+		// double avg = total / (double) elapsedTimes.size();
+		// double successRate = successCount.get() * 100.0 / CLIENT_COUNT;
+		// double avgRetry = retryCounts.stream().mapToInt(Integer::intValue).average().orElse(0);
 
-		long total = elapsedTimes.stream().mapToLong(Long::longValue).sum();
-		long max = elapsedTimes.stream().mapToLong(Long::longValue).max().orElse(0);
-		long min = elapsedTimes.stream().mapToLong(Long::longValue).min().orElse(0);
-		double avg = total / (double) elapsedTimes.size();
-		double successRate = successCount.get() * 100.0 / CLIENT_COUNT;
-		double avgRetry = retryCounts.stream().mapToInt(Integer::intValue).average().orElse(0);
-
-		System.out.println("\n[성능 요약 결과]");
-		System.out.println("총 클라이언트 수: " + CLIENT_COUNT);
-		System.out.println("락 획득 성공 수: " + successCount.get());
-		System.out.println("락 획득 성공률: " + successRate + "%");
-		System.out.println("평균 락 대기 시간(ns): " + avg);
-		System.out.println("최소 락 대기 시간(ns): " + min);
-		System.out.println("최대 락 대기 시간(ns): " + max);
-		System.out.println("평균 재시도 횟수: " + avgRetry);
-		System.out.println("실제 락 노드 번호 순서: " + acquiredNodeNames);
+		// System.out.println("\n[성능 요약 결과]");
+		// System.out.println("총 클라이언트 수: " + CLIENT_COUNT);
+		// System.out.println("락 획득 성공 수: " + successCount.get());
+		// System.out.println("락 획득 성공률: " + successRate + "%");
+		// System.out.println("평균 락 대기 시간(ns): " + avg);
+		// System.out.println("최소 락 대기 시간(ns): " + min);
+		// System.out.println("최대 락 대기 시간(ns): " + max);
+		// System.out.println("평균 재시도 횟수: " + avgRetry);
+		// System.out.println("실제 락 노드 번호 순서: " + acquiredNodeNames);
+		// System.out.println("전체 정리: 락 획득 성공 수: " + successCount.get() + ", 락 획득 성공률: " + successRate + "%" + ", 평균 재시도 횟수:" + avgRetry);
 	}
 }
