85f3e4ac26fafea6cb84dfe04c63e335b7c55973
Jayfunf <simh3077@gmail.com>
2025-06-03 22:54:19 +0900

    Update: 코드 정리

--- Enhanced_ZooKeeper/src/main/java/VersionedZooKeeperLock.java
+++ Enhanced_ZooKeeper/src/main/java/VersionedZooKeeperLock.java
@@ -1,11 +1,8 @@
 import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
 
 import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.KeeperException;
-import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooDefs;
 import org.apache.zookeeper.ZooKeeper;
 import org.apache.zookeeper.data.Stat;
@@ -34,19 +31,16 @@ public class VersionedZooKeeperLock {
 			Collections.sort(children);
 
 			int myIndex = children.indexOf(myNodeName);
-			if (myIndex == 0) {
-				System.out.println("VersionPollingLock - 락 획득 성공: " + myNodeName);
-				return myNodeName;
-			}
+			if (myIndex == 0) return myNodeName;
+
+			// Watch 등록 전 강제로 딜레이를 걸어 Watch 누락 유발
+			Thread.sleep(1000);
 
 			String prevNode = children.get(myIndex - 1);
-
-			// 앞 노드 버전 읽기
 			Stat stat = new Stat();
 			try {
 				zk.getData(lockPath + "/" + prevNode, false, stat);
 			} catch (KeeperException.NoNodeException e) {
-				// 앞 노드가 이미 삭제된 경우
 				continue;
 			}
 			int initialVersion = stat.getVersion();
